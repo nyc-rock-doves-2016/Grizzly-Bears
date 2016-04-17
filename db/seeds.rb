2718c926966e5cbbaec27fea5dfe0b8d831028a2
Ayaz Uddin <email": null>
2016-04-17 12:50:36 -0400

    css done except for stats page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
-User.create(username: 'testuser', password: 'pw')
-User.create(username: 'testuser2', password: 'pw')
+User.create(username: 'john', password: 'pw')
+User.create(username: 'josh', password: 'pw')
+User.create(username: 'ayaz', password: 'pw')
 Round.create(user_id: '1', deck_id: '1')
-Deck.create(deck_name: 'testdeck')
+Deck.create(deck_name: 'Star Wars')
+Deck.create(deck_name: 'Childish Gambino')
+Deck.create(deck_name: 'Leatherworking')
 
 
 Card.create(question: 'test question1', answer: 'test answer1', deck_id: '1')
-
 Card.create(question: 'test question2', answer: 'test answer2', deck_id: '1')
 Card.create(question: 'test question3', answer: 'test answer3', deck_id: '1')
 Card.create(question: 'test question4', answer: 'test answer4', deck_id: '1')
-
 Guess.create(content: 'test guess', card_id: '1', user_id: '1', round_id: '1')
 Guess.create(content: 'test guess2', card_id: '2', user_id: '1', round_id: '1')
 Guess.create(content: 'test guess3', card_id: '3', user_id: '1', round_id: '1')
+
+Card.create(question: 'test question1', answer: 'test answer1', deck_id: '2')
+Card.create(question: 'test question2', answer: 'test answer2', deck_id: '2')
+Card.create(question: 'test question3', answer: 'test answer3', deck_id: '2')
+Card.create(question: 'test question4', answer: 'test answer4', deck_id: '2')
+
+Card.create(question: 'test question1', answer: 'test answer1', deck_id: '3')
+Card.create(question: 'test question2', answer: 'test answer2', deck_id: '3')
+Card.create(question: 'test question3', answer: 'test answer3', deck_id: '3')
+Card.create(question: 'test question4', answer: 'test answer4', deck_id: '3')
